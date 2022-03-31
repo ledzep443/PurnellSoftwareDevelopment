837ccaa15cf1ff39ae00a831f520a8dba2a0fd12
Jimmy Purnell <jimmy@purnellsoftwaredevelopment.com>
2022-03-31 02:57:38 -0400

    Deploy changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :deploy_to, "/home/jimmy/#{fetch :application}"
 # set :pty, true
 
 # Default value for :linked_files is []
-# append :linked_files, "config/database.yml", 'config/master.key'
+append :linked_files, "config/database.yml", 'config/master.key'
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
